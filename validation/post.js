96cf5ee7e36e9296bc18b1d89f1d6c5a502b2e05
Nazam <nazamzahoor@yahoo.com>
2018-10-27 11:34:20 -0700

    image uploads done

--- validation/post.js
+++ validation/post.js
@@ -1,17 +1,28 @@
-const Validator =require('validator');
+const validator =require('validator');
 const isEmpty = require('./is-empty');
 
 module.exports = function validatePostInput(data){
     let errors = {};
 
-    data.text = !isEmpty(data.text) ? data.text : '';
+    data.title = !isEmpty(data.title) ? data.title : '';
+    data.description = !isEmpty(data.description) ? data.description : '';
+    data.price = !isEmpty(data.price) ? data.price : '';
 
-    if(!Validator.isLength(data.text, {min:2, max:300})){
-        errors.text = 'Post must be between 2 and 300 characters';
+    if(!validator.isLength(data.title, {min:2, max:20})){
+        errors.title = 'Title must be between 2 and 20 characters';
     }
-
-    if (Validator.isEmpty(data.text)) {
-        errors.text = "Text Field is Required";
+    if (validator.isEmpty(data.title)) {
+        errors.title = "Title Field is Required";
+    }
+    if (!validator.isLength(data.description, { min: 2, max: 300 })){
+        errors.description = 'descriiption Must be between 2 and 300 characters';
+    }
+    if (validator.isEmpty(data.description)){
+        errors.description = 'Description field is required';
+    }
+    if(validator.isEmpty(data.price)){
+        errors.price = 'Price field is required';
+        
     }
 
     return{
